125527e51873e555066ab956a069c8daac9258a7
RedWoodHD <Eliasganske@outlook.com>
2025-05-08 12:36:06 +0200

    Delete old trash

--- src/MiniGames/Dont_Get_Angry/Board/Field.java
+++ src/MiniGames/Dont_Get_Angry/Board/Field.java
@@ -1,26 +1,8 @@
 package MiniGames.Dont_Get_Angry.Board;
 
-import MiniGames.Dont_Get_Angry.Definition.Type;
-
 public class Field
 {
     Figure figure;
-    Type type;
-
-    public Field(Type type)
-    {
-        this.type = type;
-    }
-    public Type getType()
-    {
-        return type;
-    }
-
-    public Field setType(Type type)
-    {
-        this.type = type;
-        return this;
-    }
 
     public Figure getFigure()
     {
--- src/MiniGames/Dont_Get_Angry/Definition/Type.java
+++
@@ -1,9 +0,0 @@
-package MiniGames.Dont_Get_Angry.Definition;
-
-public enum Type
-{
-    REGULAR,
-    PROTECTET,
-    FINISH,
-    SPAWN,
-}
